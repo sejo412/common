8cd50e48c65e54e801adb535dbfcf9da5c0713b7
sinitskiy <sinitskiy@restream.rt.ru>
2019-07-12 15:48:34 +0300

    inc addresses

--- scripts/rkn/update.sh
+++ scripts/rkn/update.sh
@@ -9,12 +9,12 @@ echo "/ip firewall address-list" >> blocked.rsc
 
 iconv -f cp1251 -t utf8 z-i/dump.csv | cut -d";" -f1 | tr '|' '\n' |  tr -d ' ' | egrep -v "(Updated|^$)" | sort -uV > dirty.list
 
-./net_list_minimizer/minimize_net_list.py dirty.list 70000 | grep -v "###" > nets.list
+./net_list_minimizer/minimize_net_list.py dirty.list 85000 | grep -v "###" > nets.list
 
 rm -f dirty.list
 
 sed -i 's/\/32//g' nets.list
 sed 's/\(.*\)/add address=\1 list=rkn/g' nets.list >> blocked.rsc
 rm -f nets.list
-#scp blocked.rsc admin@192.168.88.1:/disk1/
-#ssh admin@192.168.88.1 "import /disk1/blocked.rsc"
+##scp blocked.rsc admin@192.168.88.1:/disk1/
+##ssh admin@192.168.88.1 "import /disk1/blocked.rsc"
